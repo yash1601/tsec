01f6af03099232de0783d412640fb4488b7131bc
tambibhavika2000 <46815932+tambibhavika2000@users.noreply.github.com>
2021-03-27 04:12:59 +0530

    commit

--- jobs/urls.py
+++ jobs/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from jobs import views
 
 urlpatterns = [
-    path("",views.index,name='jobs')
+    path("",views.index,name='jobs'),
+    path("dashboard/",views.dashboard,name='dashboard')
 ]
